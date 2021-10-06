7ef6661e6941bc16f4ecfc04b294b15c32c76323
Silva <matias.silva@accusys.com.ar>
2021-10-06 12:24:00 -0300

    saving project first time

---
+++ MusicApi/Controllers/SongsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicApi.Data;
+using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private ApiDBContext _dbContext;
+        public SongsController(ApiDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/<SongsController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _dbContext.Songs.ToListAsync());
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var song = await _dbContext.Songs.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            return Ok(song);
+        }
+
+        // POST api/<SongsController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Song song)
+        {
+            await _dbContext.Songs.AddAsync(song);
+            await _dbContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Song song)
+        {
+            var thisSong = await _dbContext.Songs.FindAsync(id);
+            if (thisSong == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            else
+            {
+                thisSong.Title = song.Title;
+                thisSong.Language = song.Language;
+                await _dbContext.SaveChangesAsync();
+                return Ok("Record Updated Successfully");
+            }
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var song= await _dbContext.Songs.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            else
+            {
+                _dbContext.Songs.Remove(song);
+                await _dbContext.SaveChangesAsync();
+                return Ok("Record Deleted");
+            }
+        }
+    }
+}
---
+++ MusicApi/Data/ApiDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApi.Data
+{
+    public class ApiDBContext: DbContext
+    {
+        public ApiDBContext(DbContextOptions<ApiDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Song> Songs { get; set; }
+    }
+}
